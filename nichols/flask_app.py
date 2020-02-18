68c002bb92c546bd61612fef304d57ee3bc66b7d
peterwu <peterwu19881230@gmail.com>
2020-02-17 22:03:32 -0600

    update

--- nichols/flask_app.py
+++ nichols/flask_app.py
@@ -14,16 +14,14 @@ app = Flask(__name__)
 db=yaml.safe_load(open('db.yaml'))
 
 
-'''
+
 app.config['MYSQL_HOST']=db['mysql_host']
-
-
 app.config['MYSQL_USER']=db['mysql_user']
 app.config['MYSQL_PASSWORD']=db['mysql_password']
 app.config['MYSQL_DB']=db['mysql_db']
 
 mysql=MySQL(app)
-'''
+
 
 '''
 #cur=mysql.connection.cursor
